a54ab82e1d7d914c6d03d936953206bfa11bbab1
Joel Lawrence <z3331029@unsw.edu.au>
2019-03-27 17:37:42 +1100

    sql functions built

--- Project/database.py
+++ Project/database.py
@@ -18,37 +18,77 @@ class server_database:
         return
     
     # Database operations here
-    def create_table(self, conn):
+    def create_table(self,conn):
         c = conn.cursor()
         sql = 'CREATE TABLE IF NOT EXISTS Heart (ID INTEGER PRIMARY KEY, \
-            Sex INTEGER NOT NULL, \
-                Chest_Pain_Type INTEGER NOT NULL,\
-                    RBP REAL NOT NULL, \
-                        Serum_Chol REAL NOT NULL, \
-                            Fast_Blood_Sugar INTEGER NOT NULL, \
-                                Resting_ECG INTEGER NOT NULL, \
-                                    Max_Heart_Rate REAL NOT NULL, \
-                                        EI_Angina INTEGER NOT NULL, \
-                                            Oldpeak REAL NOT NULL, \
-                                                Slope INTEGER NOT NULL, \
-                                                    Major_Vessels INTEGER NOT NULL, \
-                                                        Thal INTERGER NOT NULL, \
-                                                            Target INTEGER NOT NULL)'
+            Age Integer NOT NULL, \
+                Sex INTEGER NOT NULL, \
+                    Chest_Pain_Type INTEGER NOT NULL,\
+                        RBP REAL NOT NULL, \
+                            Serum_Chol REAL NOT NULL, \
+                                Fast_Blood_Sugar INTEGER NOT NULL, \
+                                    Resting_ECG INTEGER NOT NULL, \
+                                        Max_Heart_Rate REAL NOT NULL, \
+                                            EI_Angina INTEGER NOT NULL, \
+                                                Oldpeak REAL NOT NULL, \
+                                                    Slope INTEGER NOT NULL, \
+                                                        Major_Vessels INTEGER NOT NULL, \
+                                                            Thal INTERGER NOT NULL, \
+                                                                Target INTEGER NOT NULL)'
         c.execute(sql)
         return
 
-    def insert_row(self, conn):
-        pass
+    def insert_row(self, row):
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        try:
+            c.execute('INSERT INTO Heart VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
+        except sqlite3.Error as e:
+            print(e)
+        conn.commit()
+        conn.close()
     
-    def delete_row(self, conn):
-        pass
+    def delete_row(self, condition):
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        delete = 'DELETE From Heart WHERE {}'.format(condition)
+        try:
+            c.execute(delete)
+        except sqlite3.Error as e:
+            print(e)
+        conn.commit()
+        conn.close()
     
-    def delete_table(self, conn):
-        pass
+    def delete_table(self):
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        delete = 'DROP Heart IF EXISTS'
+        try:
+            c.execute(delete)
+        except sqlite3.Error as e:
+            print(e)
+        conn.commit()
+        conn.close()
+    
+    def select_row(self, condition):
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        delete = 'SELECT From Heart WHERE {}'.format(condition)
+        try:
+            c.execute(delete)
+        except sqlite3.Error as e:
+            print(e)
+        conn.commit()
+        conn.close()
     
     # Import/clean operations
-    def import_data(self, conn, data):
-        pass
+    def import_data(self, data):
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        rows = []
+        c.executemany("INSERT INTO Heart VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", rows)
+        conn.commit()
+        conn.close()
 
 # Test area
-db = database()
+db = server_database()
