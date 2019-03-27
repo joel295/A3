f8e07f611bf1f4c8cf7cc5ec8e8460a52eb78108
Joel Lawrence <z3331029@unsw.edu.au>
2019-03-27 16:57:22 +1100

    template fix

--- Project/database.py
+++ Project/database.py
@@ -2,28 +2,31 @@ import sqlite3
 import pandas
 
 # class to perform database operations
+
 class database:
+    # init class
     def __init__(self):
         self.database_name = 'database.db'
         try:
-            conn = sqlite3.connect(filename)
-            self.database = filename
+            conn = sqlite3.connect(self.database_name)
             self.create_table(conn)
-        except Error as e:
-        print(e)
+        except sqlite3.Error as e:
+            print(e)
         finally:
             conn.commit()
             conn.close()
-
+        return
+    
+    # Database operations here
     def create_table(self, conn):
         c = conn.cursor()
-        sql = 'CREATE TABLE IF NOT EXISTS Heart (Index INTEGER PRIMARY KEY, \
+        sql = 'CREATE TABLE IF NOT EXISTS Heart (ID INTEGER PRIMARY KEY, \
             Sex INTEGER NOT NULL, \
                 Chest_Pain_Type INTEGER NOT NULL,\
                     RBP REAL NOT NULL, \
                         Serum_Chol REAL NOT NULL, \
                             Fast_Blood_Sugar INTEGER NOT NULL, \
-                                Resting_ECG INTEGER NOT NULL\
+                                Resting_ECG INTEGER NOT NULL, \
                                     Max_Heart_Rate REAL NOT NULL, \
                                         EI_Angina INTEGER NOT NULL, \
                                             Oldpeak REAL NOT NULL, \
@@ -32,17 +35,19 @@ class database:
                                                         Thal INTERGER NOT NULL, \
                                                             Target INTEGER NOT NULL)'
         c.execute(sql)
-    
-    def self.insert_row(self, conn):
-        pass
+        return
 
-    def self.delete_row(self, conn):
+    def insert_row(self, conn):
         pass
     
-    def self.update_row(self, conn):
+    def delete_row(self, conn):
         pass
     
-    def self.import_data(self, conn, data):
+    def delete_table(self, conn):
+        pass
+    
+    # Import/clean operations
+    def import_data(self, conn, data):
         pass
 
 # Test area
