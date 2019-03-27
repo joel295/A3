e5bf2b5a4f914d7f57d6ad290a80eb013b3c75e0
Joel Lawrence <z3331029@unsw.edu.au>
2019-03-27 16:41:50 +1100

    db template

--- Project/database.py
+++ Project/database.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas
 
+# class to perform database operations
 class database:
     def __init__(self):
         self.database_name = 'database.db'
@@ -16,5 +17,33 @@ class database:
 
     def create_table(self, conn):
         c = conn.cursor()
-        #sql = 'CREATE TABLE IF NOT EXISTS <I NEED A NAME> (<I NEED FIELDS>)'
-        #c.execute(sql)
+        sql = 'CREATE TABLE IF NOT EXISTS Heart (Index INTEGER PRIMARY KEY, \
+            Sex INTEGER NOT NULL, \
+                Chest_Pain_Type INTEGER NOT NULL,\
+                    RBP REAL NOT NULL, \
+                        Serum_Chol REAL NOT NULL, \
+                            Fast_Blood_Sugar INTEGER NOT NULL, \
+                                Resting_ECG INTEGER NOT NULL\
+                                    Max_Heart_Rate REAL NOT NULL, \
+                                        EI_Angina INTEGER NOT NULL, \
+                                            Oldpeak REAL NOT NULL, \
+                                                Slope INTEGER NOT NULL, \
+                                                    Major_Vessels INTEGER NOT NULL, \
+                                                        Thal INTERGER NOT NULL, \
+                                                            Target INTEGER NOT NULL)'
+        c.execute(sql)
+    
+    def self.insert_row(self, conn):
+        pass
+
+    def self.delete_row(self, conn):
+        pass
+    
+    def self.update_row(self, conn):
+        pass
+    
+    def self.import_data(self, conn, data):
+        pass
+
+# Test area
+db = database()
