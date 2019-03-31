64ca9f2bca4d67571aa15909f36005d21c4da05a
Joel Lawrence <z3331029@unsw.edu.au>
2019-03-31 18:29:05 +1100

    draft website ready

---
+++ Project/Server/server.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, abort
+from server_helper import graph_dict
+
+def server():
+    app = Flask(__name__)
+    app.debug =  True
+    app.host='localhost'
+    app.config['TESTING']
+    return app
+
+server = server()
+
+@server.errorhandler(404)
+def page_not_found(e):
+    return render_template("bad.html"), 404
+
+@server.route('/')
+def home_page():
+    return render_template("home.html")
+
+@server.route('/graph')
+@server.route('/graph<int:number>')
+def graph(number=None):
+    if number == None:
+        return render_template("graphw.html")
+    try:
+        number = int(number)
+        graph_name = graph_dict[number]
+    except (ValueError, KeyError):
+        abort(404)
+    if not 1 <= number <= 14:
+        abort(404)
+    else:
+        return render_template("graph.html", graph_name = graph_name)
+
+@server.route('/factors')
+def factors():
+    return render_template("factors.html")
+
+@server.route('/predictor')
+def predictor():
+    return render_template("predictor.html")
+
+@server.route('/other')
+def other():
+    return render_template("other.html")
+
+server.run()
---
+++ Project/Server/server_helper.py
@@ -0,0 +1,24 @@
+graph_dict = {1:'Age',2:'Sex',3:'Chest Pain Type',4:'Resting Blood Pressure',\
+    5:'Serum Cholestrol',6:'Fasting Blood Sugar',7:'Resting ECG Results',\
+        8:'Max. Heart Rate', 9:'Exercise Induced Angina', 10:'oldpeak',\
+            11:'Peak Exercise ST slope',12:'Major vessels by flourosopy',\
+                13:'thal',14:'target'}
+
+'''
+<li class="active"></li>
+<li><a href="/">Home</a></li>
+<li><a href="/graph?=1">Age</a></li>
+<li><a href="/graph?=2">Sex</a></li>
+<li><a href="/graph?=3">Chest Pain Type</a></li>
+<li><a href="/graph?=4">Resting Blood Pressure</a></li>
+<li><a href="/graph?=5">Serum Cholestrol</a></li>
+<li><a href="/graph?=6">Fasting Blood Sugar</a></li>
+<li><a href="/graph?=7">Resting ECG Results</a></li>
+<li><a href="/graph?=8">Max. Heart Rate</a></li>
+<li><a href="/graph?=9">Exercise Induced Angina</a></li>
+<li><a href="/graph?=10">oldpeak</a></li>
+<li><a href="/graph?=11">Peak Exercise ST slope</a></li>
+<li><a href="/graph?=12">Major vessels by flourosopy</a></li>
+<li><a href="/graph?=13">thal</a></li>
+<li><a href="/graph?=14">target</a></li>
+'''
