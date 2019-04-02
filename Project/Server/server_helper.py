2fef117d5268fe512aab3286508e361d7c429a36
Joel Lawrence <z3331029@unsw.edu.au>
2019-04-02 19:42:32 +1100

    basic layout additions

--- Project/Server/server_helper.py
+++ Project/Server/server_helper.py
@@ -24,22 +24,3 @@ def create_plot():
     plt.savefig('static/images/test.png')
 
 create_plot()
-
-'''
-<li class="active"></li>
-<li><a href="/">Home</a></li>
-<li><a href="/graph?=1">Age</a></li>
-<li><a href="/graph?=2">Sex</a></li>
-<li><a href="/graph?=3">Chest Pain Type</a></li>
-<li><a href="/graph?=4">Resting Blood Pressure</a></li>
-<li><a href="/graph?=5">Serum Cholestrol</a></li>
-<li><a href="/graph?=6">Fasting Blood Sugar</a></li>
-<li><a href="/graph?=7">Resting ECG Results</a></li>
-<li><a href="/graph?=8">Max. Heart Rate</a></li>
-<li><a href="/graph?=9">Exercise Induced Angina</a></li>
-<li><a href="/graph?=10">oldpeak</a></li>
-<li><a href="/graph?=11">Peak Exercise ST slope</a></li>
-<li><a href="/graph?=12">Major vessels by flourosopy</a></li>
-<li><a href="/graph?=13">thal</a></li>
-<li><a href="/graph?=14">target</a></li>
-'''
