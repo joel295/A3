cb999e929b334289638bc085b5dcba598bebda0e
kln80r <kln80r@gmail.com>
2019-04-07 23:14:28 +1000

    Thalassemia

--- Project/Visualization/graph.py
+++ Project/Visualization/graph.py
@@ -360,6 +360,52 @@ class graph:
         plt.savefig(IMG_PATH + 'graph9.png')
 
 
+    # Thalassemia
+    def create_plot_13(self):
+
+        # Get data
+        conn = sqlite3.connect(db.database_name)
+        df = pd.read_sql_query('SELECT Age, Sex, Thal FROM Heart;', conn)
+
+        ## Plot histograms male and female by type
+
+        sns.set_style("darkgrid")               # set the background of the charts
+        bins = [20,30,40,50,60,70,80]           # group age by 10's
+        palette = {'color': ['b', 'r']}         # separate colours for male/female
+
+        g = sns.FacetGrid(df, row='Sex', col='Thal', hue='Sex',
+                margin_titles=True, hue_kws=palette)
+        g.map(sns.distplot, 'Age', bins=bins, kde=False,
+                hist_kws=dict(edgecolor="black", linewidth=1));
+
+        ## Format Plot
+
+        # Axis Limits
+        g.axes[1,0].set_ylim(0,61)
+        g.axes[1,0].set_xlim(20,80)
+        g.axes[1,0].set_xticks(range(20, 80, 10))
+        g.axes[1,0].set_yticks(range(0, 55, 10))
+
+        # Titles
+        plt.subplots_adjust(top=0.85, left=0.07, bottom=0.15)
+        g.fig.suptitle("Thalassemia\n")
+        g.axes[0,0].set_title("Normal")
+        g.axes[0,1].set_title("Fixed Defect")
+        g.axes[0,2].set_title("Reversible Defect")
+
+        # Overall Labels
+        g.set_axis_labels('', '')
+        g.fig.text(x=0.01, y=0.5, verticalalignment='center',s='Frequency', rotation=90, size=11)
+        g.fig.text(x=0.5, y=0.01, horizontalalignment='center', s='Age', size=11)
+        gender_labels =  ["","","Male","","","Female"]
+        for i, ax in enumerate(g.axes.flat):
+            plt.setp(ax.texts, text=gender_labels[i], size=11)
+
+        g.set_xticklabels(rotation=45, labels=["20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "> 80"])
+
+        # Save image
+        plt.savefig(IMG_PATH + 'graph13.png')
+
 # Main function to create each graph and save in /Server/static/images/
 if __name__ == '__main__':
 
@@ -392,11 +438,11 @@ if __name__ == '__main__':
     # Max Heart Rate
     g8 = graph(8, db)
     g8.create_plot_8()
-    '''
 
     # Exercise Induced Angina
     g9 = graph(9, db)
     g9.create_plot_9()
+    '''
 
     # Oldpeak
 
@@ -405,3 +451,5 @@ if __name__ == '__main__':
     # Number Vessels Coloure by Flouroscopy
 
     # Thalassemia
+    g13 = graph(13, db)
+    g13.create_plot_13()
